cad5dd3488f81b7b2ebd7e319a296d02d5e1e12f
chethiyaE <chethiyaediri@gmail.com>
2015-11-01 19:35:28 +0530

    partial views added and index changed

--- webpage.js
+++ webpage.js
@@ -42,6 +42,7 @@ app.use(cookieParser()); // read cookies (needed for auth)
 app.use(bodyParser.json()); // get information from html forms
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.set('views', path.join(__dirname, 'views/pages'));
 app.set('view engine', 'ejs'); // set up ejs for templating
 
 app.use(express.static(__dirname + '/public'));
