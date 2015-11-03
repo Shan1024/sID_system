d4adaa5c518962c92ea66f535c33094b6b985a43
shan1024 <gambit1024@gmail.com>
2015-11-03 12:22:38 +0530

    Sign up changed

---
+++ public/scripts/loadFBPage.js
@@ -0,0 +1,169 @@
+/**
+ * Created by Shan on 11/2/2015.
+ */
+
+
+
+
+$(function () {
+    console.log("Hello World");
+
+    var $url = $('#url');
+    var id = $url.attr('data-id');
+
+    console.log("id: " + id);
+
+
+
+    //$.ajax ({
+    //    type: 'GET',
+    //    url: "localhost:8080/?url=www.google.com",
+    //    //'https://www.facebook.com/'+id,
+    //    success: function(page){
+    //        console.log('success', page);
+    //        //$.each(page, function(i, s){
+    //        //
+    //        //});
+    //    },
+    //    error: function(){
+    //        alert('Error loading data');
+    //    }
+    //});
+
+    //var url = 'https://www.facebook.com/'+id;
+
+    //$.ajax({
+    //    type: 'GET',
+    //    url: url,
+    //    async: false,
+    //    jsonpCallback: 'jsonCallback',
+    //    contentType: "application/json",
+    //    dataType: 'jsonp',
+    //    success: function(json) {
+    //        console.dir(json.sites);
+    //    },
+    //    error: function(e) {
+    //        console.log(e.message);
+    //    }
+    //});
+
+    //$.ajax({
+    //    url: url,
+    //    dataType: 'jsonp',
+    //    success: function(dataWeGotViaJsonp){
+    //        console.log(dataWeGotViaJsonp)
+    //        //var text = '';
+    //        //var len = dataWeGotViaJsonp.length;
+    //        //for(var i=0;i<len;i++){
+    //        //    twitterEntry = dataWeGotViaJsonp[i];
+    //        //    text += '<p><img src = "' + twitterEntry.user.profile_image_url_https +'"/>' + twitterEntry['text'] + '</p>'
+    //        //}
+    //        //$('#twitterFeed').html(text);
+    //    }
+    //});
+
+
+    //$.get("localhost:8080/?url=www.google.com", function(response) {
+    //    alert(response)
+    //});
+
+    $.ajax({
+
+        // The 'type' property sets the HTTP method.
+        // A value of 'PUT' or 'DELETE' will trigger a preflight request.
+        type: 'GET',
+
+        // The URL to make the request to.
+        url: 'https://www.google.com',
+
+        // The 'contentType' property sets the 'Content-Type' header.
+        // The JQuery default for this property is
+        // 'application/x-www-form-urlencoded; charset=UTF-8', which does not trigger
+        // a preflight. If you set this value to anything other than
+        // application/x-www-form-urlencoded, multipart/form-data, or text/plain,
+        // you will trigger a preflight request.
+        contentType: 'text/plain',
+
+        xhrFields: {
+            // The 'xhrFields' property sets additional fields on the XMLHttpRequest.
+            // This can be used to set the 'withCredentials' property.
+            // Set the value to 'true' if you'd like to pass cookies to the server.
+            // If this is enabled, your server must respond with the header
+            // 'Access-Control-Allow-Credentials: true'.
+            withCredentials: false
+        },
+
+        headers: {
+            // Set any custom headers here.
+            // If you set any non-simple headers, your server must include these
+            // headers in the 'Access-Control-Allow-Headers' response header.
+        },
+
+        success: function() {
+            // Here's where you handle a successful response.
+            console.log("OK");
+        },
+
+        error: function() {
+            // Here's where you handle an error response.
+            // Note that if the error was due to a CORS issue,
+            // this function will still fire, but there won't be any additional
+            // information about the error.
+            console.log("error");
+        }
+    });
+
+});
+
+
+
+//// Create the XHR object.
+//function createCORSRequest(method, url) {
+//    var xhr = new XMLHttpRequest();
+//    if ("withCredentials" in xhr) {
+//        // XHR for Chrome/Firefox/Opera/Safari.
+//        xhr.open(method, url, true);
+//    } else if (typeof XDomainRequest != "undefined") {
+//        // XDomainRequest for IE.
+//        xhr = new XDomainRequest();
+//        xhr.open(method, url);
+//    } else {
+//        // CORS not supported.
+//        xhr = null;
+//    }
+//    return xhr;
+//}
+//
+//// Helper method to parse the title tag from the response.
+//function getTitle(text) {
+//    return text.match('<title>(.*)?</title>')[1];
+//}
+//
+//// Make the actual CORS request.
+//function makeCorsRequest() {
+//    // All HTML5 Rocks properties support CORS.
+//    var url = 'https://www.facebook.com';
+//
+//    var xhr = createCORSRequest('GET', url);
+//    if (!xhr) {
+//        alert('CORS not supported');
+//        return;
+//    }
+//
+//    // Response handlers.
+//    xhr.onload = function() {
+//        var text = xhr.responseText;
+//        var title = getTitle(text);
+//        alert('Response from CORS request to ' + url + ': ' + title);
+//    };
+//
+//    xhr.onerror = function() {
+//        alert('Woops, there was an error making the request.');
+//    };
+//
+//    xhr.send();
+//}
+//
+//var url = 'https://www.google.com';
+//var xhr = createCORSRequest('GET', url);
+//xhr.send();
--- webpage.js
+++ webpage.js
@@ -3,20 +3,20 @@
 // set up ======================================================================
 // get all the tools we need
 
-var express  = require('express');
-var app      = express();
+var express = require('express');
+var app = express();
 var mongoose = require('mongoose');
 var passport = require('passport');
-var flash    = require('connect-flash');
+var flash = require('connect-flash');
 var path = require('path');
 var https = require('https');
 var http = require('http');
 var fs = require('fs');
 var chalk = require('chalk');
-var morgan       = require('morgan');
+var morgan = require('morgan');
 var cookieParser = require('cookie-parser');
-var bodyParser   = require('body-parser');
-var session      = require('express-session');
+var bodyParser = require('body-parser');
+var session = require('express-session');
 
 var config = require('./config/config'); // get our config file
 
@@ -40,7 +40,7 @@ app.set('password', config.password);
 app.use(morgan('dev')); // log every request to the console
 app.use(cookieParser()); // read cookies (needed for auth)
 app.use(bodyParser.json()); // get information from html forms
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.set('views', path.join(__dirname, 'views/pages'));
 app.set('view engine', 'ejs'); // set up ejs for templating
@@ -58,8 +58,9 @@ app.use(flash()); // use connect-flash for flash messages stored in session
 
 // routes ======================================================================
 require('./app/routes/webRoutes.js')(app, passport); // load our routes and pass in our app and fully configured passport
+//require('./app/routes/secureRoutes.js')(app, express);
+require('./app/routes/testRoutes.js')(app, express);
 require('./app/routes/dummyRoutes.js')(app, express);
-
 // launch ======================================================================
 //app.listen(port);
 //console.log('The magic happens on port ' + port);
@@ -75,37 +76,37 @@ console.log(chalk.green("https server started at port " + config.httpsPort));
 
 //*********************
 /*
-var https = require('https');
-var fs = require('fs');
+ var https = require('https');
+ var fs = require('fs');
 
-var options = {
-  key: fs.readFileSync('key.pem'),
-  cert: fs.readFileSync('cert.pem')
-};
+ var options = {
+ key: fs.readFileSync('key.pem'),
+ cert: fs.readFileSync('cert.pem')
+ };
 
-var a = https.createServer(options, function (req, res) {
-  res.writeHead(200);
-  res.end("hello world\n");
-}).listen(8080);
-*/
+ var a = https.createServer(options, function (req, res) {
+ res.writeHead(200);
+ res.end("hello world\n");
+ }).listen(8080);
+ */
 
 //****************
 /*
-var fs = require('fs');
-var http = require('http');
-var https = require('https');
-var privateKey  = fs.readFileSync('key.pem', 'utf8');
-var certificate = fs.readFileSync('cert.pem', 'utf8');
+ var fs = require('fs');
+ var http = require('http');
+ var https = require('https');
+ var privateKey  = fs.readFileSync('key.pem', 'utf8');
+ var certificate = fs.readFileSync('cert.pem', 'utf8');
 
-var credentials = {key: privateKey, cert: certificate};
-var express = require('express');
-var app = express();
+ var credentials = {key: privateKey, cert: certificate};
+ var express = require('express');
+ var app = express();
 
-// your express configuration here
+ // your express configuration here
 
-var httpServer = http.createServer(app);
-var httpsServer = https.createServer(credentials, app);
+ var httpServer = http.createServer(app);
+ var httpsServer = https.createServer(credentials, app);
 
-httpServer.listen(8000);
-httpsServer.listen(8080);
-*/
+ httpServer.listen(8000);
+ httpsServer.listen(8080);
+ */
