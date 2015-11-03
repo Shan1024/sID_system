a1620c8e4c29eb6cd4e329adfc5d4e017fe409cd
shan1024 <gambit1024@gmail.com>
2015-11-04 03:22:51 +0530

    facebook account is not linked problem fixed

--- config/facebook.js
+++ config/facebook.js
@@ -1,7 +1,7 @@
 var https = require('https');
 var User = require('../app/models/user');
 
-exports.getFbData = function(req, apiPath, callback) {
+exports.getFbData = function (req, apiPath, callback) {
 
     User.findById(req.user._id)
         .populate('userDetails.facebook')
@@ -10,31 +10,32 @@ exports.getFbData = function(req, apiPath, callback) {
         .exec(function (error, user) {
             console.log(JSON.stringify(user, null, "\t"));
 
-            var accessToken = user.userDetails.facebook.token;
+            if (user) {
+                var accessToken = user.userDetails.facebook.token;
 
-            var options = {
-                host: 'graph.facebook.com',
-                port: 443,
-                path: apiPath + '?access_token=' + accessToken, //apiPath example: '/me/friends'
-                method: 'GET'
-            };
+                var options = {
+                    host: 'graph.facebook.com',
+                    port: 443,
+                    path: apiPath + '?access_token=' + accessToken, //apiPath example: '/me/friends'
+                    method: 'GET'
+                };
 
-            var buffer = ''; //this buffer will be populated with the chunks of the data received from facebook
-            var request = https.get(options, function(result){
-                result.setEncoding('utf8');
-                result.on('data', function(chunk){
-                    buffer += chunk;
+                var buffer = ''; //this buffer will be populated with the chunks of the data received from facebook
+                var request = https.get(options, function (result) {
+                    result.setEncoding('utf8');
+                    result.on('data', function (chunk) {
+                        buffer += chunk;
+                    });
+
+                    result.on('end', function () {
+                        callback(buffer);
+                    });
                 });
 
-                result.on('end', function(){
-                    callback(buffer);
+                request.on('error', function (e) {
+                    console.log('error from facebook.getFbData: ' + e.message)
                 });
-            });
-
-            request.on('error', function(e){
-                console.log('error from facebook.getFbData: ' + e.message)
-            });
-
+            }
             request.end();
 
             //res.render('partials/profile', {user: user});
