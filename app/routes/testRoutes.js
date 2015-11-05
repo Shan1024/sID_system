1586a1470a4444ecd87a3e99d57a1eed4c557d40
shan1024 <gambit1024@gmail.com>
2015-11-06 03:30:49 +0530

    addRating route added

--- app/routes/testRoutes.js
+++ app/routes/testRoutes.js
@@ -65,8 +65,8 @@ module.exports = function (app, express) {
             var user = new User({
                 userDetails: {
                     local: {
-                        firstname:firstname,
-                        lastname:lastname,
+                        firstname: firstname,
+                        lastname: lastname,
                         email: email,
                         password: password
                     }
@@ -80,6 +80,105 @@ module.exports = function (app, express) {
                     res.json({message: "Failed 2"});
                 } else {
                     res.json(user);
+                }
+            });
+
+        });
+
+    testRouter.route('/addRating')
+        .post(function (req, res) {
+
+            var myid = req.body.myid;
+            var targetid = req.body.targetid;
+            var claim = req.body.claim;
+            var rating = req.body.rating;
+
+            Facebook.findOne({
+                id: myid
+            }, function (err, me) {
+                if (me) {
+                    console.log(chalk.yellow("User found: " + JSON.stringify(me, null, "\t")));
+
+                    Facebook.findOne({
+                        id: targetid
+                    }, function (err, target) {
+                        if (target) {
+
+                            console.log(chalk.blue("Target found: " + JSON.stringify(target, null, "\t")));
+
+                            User.findOne({
+                                _id: me.user,
+                                'facebook.ratedByMe': {$elemMatch: {targetid: target._id}}
+                            }, function (err, user) {
+                                if (user) {
+                                    console.log(chalk.green('Rating already available'));
+                                    console.log(chalk.green(JSON.stringify(user, null, "\t")));
+                                    console.log("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+                                    res.json({message: "Rating already available"});
+
+                                } else {
+                                    console.log(chalk.yellow('Rating not available'));
+
+
+                                    //User.findOne({
+                                    //    _id: me.user
+                                    //}, function (err, me) {
+                                    //    console.log(chalk.cyan('Me: ' + JSON.stringify(me, null, "\t")));
+                                    //
+                                    //
+                                    //});
+
+                                    var entry = {
+                                        targetid: target._id
+                                    };
+
+                                    User.findOneAndUpdate({
+                                            _id: me.user
+                                        },
+                                        {
+                                            $push: {
+                                                'facebook.ratedByMe': {
+                                                    targetid: target._id,
+                                                    entries: {
+                                                        $push: {
+                                                            'facebook.ratedByMe.entries.basic_info': entry
+                                                        }
+                                                    }
+                                                }
+                                            }
+                                        },
+                                        {
+                                            safe: true,
+                                            upsert: true
+                                        },
+                                        function (err, model) {
+
+                                            if (err) {
+                                                console.log(chalk.red(err));
+                                                res.json({message: err});
+                                            }
+
+                                            console.log("Model: " + model);
+
+
+                                            res.json({message: "OK"});
+                                        }
+                                    );
+
+                                }
+                            });
+
+                        } else {
+                            console.log(chalk.red("Target not found"));
+                            res.json({message: "Target not found"});
+                        }
+
+                    });
+
+
+                } else {
+                    console.log(chalk.red("Me not found"));
+                    res.json({message: "Me not found"});
                 }
             });
 
