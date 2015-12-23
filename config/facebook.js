705efb611a13d09dd5bbb62bdf3edab24de22184
shan1024 <gambit1024@gmail.com>
2015-12-23 21:57:21 +0530

    getFbName function added

--- config/facebook.js
+++ config/facebook.js
@@ -39,4 +39,35 @@ exports.getFbData = function (req, apiPath, callback) {
 
             //res.render('partials/profile', {user: user});
         });
-}
+};
+
+exports.getFbName = function (targetUser, apiPath, callback) {
+
+    var accessToken = targetUser.userDetails.facebook.token;
+    console.log("accessToken: " + accessToken);
+
+    var options = {
+        host: 'graph.facebook.com',
+        port: 443,
+        path: apiPath + '?access_token=' + accessToken, //apiPath example: '/me/friends'
+        method: 'GET'
+    };
+
+    var buffer = ''; //this buffer will be populated with the chunks of the data received from facebook
+    var request = https.get(options, function (result) {
+        result.setEncoding('utf8');
+        result.on('data', function (chunk) {
+            buffer += chunk;
+        });
+
+        result.on('end', function () {
+            callback(buffer);
+        });
+    });
+
+    request.on('error', function (e) {
+        console.log('error from facebook.getFbData: ' + e.message)
+    });
+    request.end();
+
+};
