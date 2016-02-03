e957a0bfe7bbc40aed43d53cd0de689ee46e9679
shan1024 <gambit1024@gmail.com>
2016-02-03 22:16:17 +0530

    New chrome and firefox images added

--- public/scripts/loadFBPage.js
+++ public/scripts/loadFBPage.js
@@ -8,16 +8,16 @@
 $(function () {
     console.log("Hello World");
 
-    var $url = $('#url');
-    var id = $url.attr('data-id');
-
-    console.log("id: " + id);
-
-
-
+    //var $url = $('#url');
+    //var id = $url.attr('data-id');
+    //
+    //console.log("id: " + id);
+    //
+    //
+    //
     //$.ajax ({
     //    type: 'GET',
-    //    url: "localhost:8080/?url=www.google.com",
+    //    url: "https://www.facebook.com",
     //    //'https://www.facebook.com/'+id,
     //    success: function(page){
     //        console.log('success', page);
@@ -29,6 +29,35 @@ $(function () {
     //        alert('Error loading data');
     //    }
     //});
+
+
+
+
+
+    var settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": "https://www.facebook.com/",
+        "method": "GET",
+        "headers": {
+            "cache-control": "no-cache",
+            "postman-token": "ca2235ac-4f84-517c-0128-859bcaeb97bb"
+        }
+    }
+
+    $.ajax(settings).done(function (response) {
+        console.log(response);
+    });
+
+
+
+
+
+
+
+
+
+
 
     //var url = 'https://www.facebook.com/'+id;
 
@@ -67,52 +96,52 @@ $(function () {
     //    alert(response)
     //});
 
-    $.ajax({
-
-        // The 'type' property sets the HTTP method.
-        // A value of 'PUT' or 'DELETE' will trigger a preflight request.
-        type: 'GET',
-
-        // The URL to make the request to.
-        url: 'https://www.google.com',
-
-        // The 'contentType' property sets the 'Content-Type' header.
-        // The JQuery default for this property is
-        // 'application/x-www-form-urlencoded; charset=UTF-8', which does not trigger
-        // a preflight. If you set this value to anything other than
-        // application/x-www-form-urlencoded, multipart/form-data, or text/plain,
-        // you will trigger a preflight request.
-        contentType: 'text/plain',
-
-        xhrFields: {
-            // The 'xhrFields' property sets additional fields on the XMLHttpRequest.
-            // This can be used to set the 'withCredentials' property.
-            // Set the value to 'true' if you'd like to pass cookies to the server.
-            // If this is enabled, your server must respond with the header
-            // 'Access-Control-Allow-Credentials: true'.
-            withCredentials: false
-        },
-
-        headers: {
-            // Set any custom headers here.
-            // If you set any non-simple headers, your server must include these
-            // headers in the 'Access-Control-Allow-Headers' response header.
-        },
-
-        success: function() {
-            // Here's where you handle a successful response.
-            console.log("OK");
-        },
-
-        error: function() {
-            // Here's where you handle an error response.
-            // Note that if the error was due to a CORS issue,
-            // this function will still fire, but there won't be any additional
-            // information about the error.
-            console.log("error");
-        }
-    });
-
+//    $.ajax({
+//
+//        // The 'type' property sets the HTTP method.
+//        // A value of 'PUT' or 'DELETE' will trigger a preflight request.
+//        type: 'GET',
+//
+//        // The URL to make the request to.
+//        url: 'https://www.google.com',
+//
+//        // The 'contentType' property sets the 'Content-Type' header.
+//        // The JQuery default for this property is
+//        // 'application/x-www-form-urlencoded; charset=UTF-8', which does not trigger
+//        // a preflight. If you set this value to anything other than
+//        // application/x-www-form-urlencoded, multipart/form-data, or text/plain,
+//        // you will trigger a preflight request.
+//        contentType: 'text/plain',
+//
+//        xhrFields: {
+//            // The 'xhrFields' property sets additional fields on the XMLHttpRequest.
+//            // This can be used to set the 'withCredentials' property.
+//            // Set the value to 'true' if you'd like to pass cookies to the server.
+//            // If this is enabled, your server must respond with the header
+//            // 'Access-Control-Allow-Credentials: true'.
+//            withCredentials: false
+//        },
+//
+//        headers: {
+//            // Set any custom headers here.
+//            // If you set any non-simple headers, your server must include these
+//            // headers in the 'Access-Control-Allow-Headers' response header.
+//        },
+//
+//        success: function() {
+//            // Here's where you handle a successful response.
+//            console.log("OK");
+//        },
+//
+//        error: function() {
+//            // Here's where you handle an error response.
+//            // Note that if the error was due to a CORS issue,
+//            // this function will still fire, but there won't be any additional
+//            // information about the error.
+//            console.log("error");
+//        }
+//    });
+//
 });
 
 
